9ee5fe20b795eae86ea6228798683e781361f668
OybekBoltaboyev <oybekboltaboyev1443@gmail.com>
2024-06-24 05:45:15 -0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,18 @@
+"""variable="Hello World! "
+print(variable)
+"""
+'''xabar="yuklangan matn"
+print(xabar)
+xabar="yangi matn"
+print(xabar)'''
+
+'''radius = 5
+pi = 3.14159
+yuza = pi*radius**2
+print("Radiusi", radius, "ga teng doira yuzi=", yuza)'''
+
+kocha="Bog'bon"
+mahalla="Sag'bon"
+tuman="Bodomzor"
+viloyat="Samarqand"
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
